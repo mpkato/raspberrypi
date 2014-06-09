5dd4557668b908f779da7dfc8364e5b0bebacbd9
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2014-06-09 18:40:14 +0900

    add basics

---
+++ basics.py
@@ -0,0 +1,26 @@
+# strings
+txt = 'Hello,'
+txt += ' World! '
+print txt
+print txt * 2
+print
+
+# int
+i = 0
+i = i + 2
+print i
+print
+
+# float
+f = 0.5
+f = f * 2
+print f
+print
+
+# int and float in strings
+print '%d == 2' % i
+print '%s == 2' % i
+print '%f == 1.000000' % f
+print '%d == 1' % f
+print '%s == 1.0' % f
+print '%d + %d == 3' % (i, f)
